526cf20ac1f53f30625bb2f41ef189f902bc3f05
okcarlosjose <7110293+okcarlosjose@users.noreply.github.com>
2022-06-06 17:08:12 -0500

    updated Alchemy keys

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ function notificationReceived(req) {
 // add an address to a notification in Alchemy
 async function addAddress(new_address) {
   console.log("adding address " + new_address);
-  const body = { webhook_id: "wh_1blwzaor8powfns3", addresses_to_add: [new_address], addresses_to_remove: [] };
+  const body = { webhook_id: "wh_mtbut8e6grfm955j", addresses_to_add: [new_address], addresses_to_remove: [] };
   try {
     fetch('https://dashboard.alchemyapi.io/api/update-webhook-addresses', {
       method: 'PATCH',
